feb45e1059727799647eb021d35abf3b29490cb6
subbaSahil <sahil_s@pursuitsoftware.com>
2025-02-10 20:19:23 +0530

    testing

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,9 +1,18 @@
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class SeleniumTest extends BaseTest {
-    @Test
+    @Test(priority = 1)
     public void helloWorld(){
         HelloPage hp = new HelloPage();
         hp.helloWorld();
+    }
+    @Test(priority = 2 )
+    public void contactUsTest() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,100)");
+
+        ContactUsButtonPage contactUs = new ContactUsButtonPage(driver);
+        contactUs.test();
     }
 }
